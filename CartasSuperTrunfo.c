0560abd255e6d8c405c9ccc43ba2f99d8cc68d94
Daniela Carlini <danicarlini@hotmail.com>
2026-04-22 17:27:06 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,58 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+    char estado1, estado2;
+    char codigo1[10], codigo2[10];   // espaço para códigos
+    char nome1[50], nome2[50];       // espaço para nomes das Cidades
+    int pop1, pop2;
+    float area1, area2;
+    float pib1, pib2;
+    int pontos1, pontos2;
   // Área para entrada de dados
+    printf("--- Cadastro Carta 1 ---\n");
+    printf("Estado (A-H): ");
+    scanf(" %c", &estado1);
+    printf("Código (ex: A01): ");
+    scanf(" %s", codigo1);
+    printf("Nome da Cidade: ");
+    scanf(" %[^\n]", nome1);   // aceita espaços
+    printf("População: ");
+    scanf("%d", &pop1);
+    printf("Área (km²): ");
+    scanf("%f", &area1);
+    printf("PIB (bilhões): ");
+    scanf("%f", &pib1);
+    printf("Pontos Turísticos: ");
+    scanf("%d", &pontos1);
 
+    printf("\n--- Cadastro Carta 2 ---\n");
+    printf("Estado (A-H): ");
+    scanf(" %c", &estado2);
+    printf("Código (ex: A01): ");
+    scanf(" %s", codigo2);
+    printf("Nome da Cidade: ");
+    scanf(" %[^\n]", nome2);   // aceita espaços
+    printf("População: ");
+    scanf("%d", &pop2);
+    printf("Área (km²): ");
+    scanf("%f", &area2);
+    printf("PIB (bilhões): ");
+    scanf("%f", &pib2);
+    printf("Pontos Turísticos: ");
+    scanf("%d", &pontos2);
   // Área para exibição dos dados da cidade
+    printf("\n--- Cartas Cadastradas ---\n");
 
+    printf("Carta 1: %c - %s\n", estado1, codigo1);
+    printf("Nome da Cidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nPontos turísticos: %d\n",
+           nome1, pop1, area1, pib1, pontos1);
+
+    printf("=============================\n");
+
+    printf("Carta 2: %c - %s\n", estado2, codigo2);
+    printf("Nome da Cidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nPontos turísticos: %d\n",
+           nome2, pop2, area2, pib2, pontos2);
+
+    printf("=============================\n");
 return 0;
 } 
